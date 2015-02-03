e363085b6823f75c0b121d38176b3a55caabb975
BradCB <brad_c_bolton.bolton@aggiemail.usu.edu>
2015-02-02 19:29:06 -0700

    This program is the header file for the hash table class

---
+++ hash.h
@@ -0,0 +1,37 @@
+//Brad Bolton A01383991
+//This program is the header file for the hash table class
+#include<iostream>
+#include<string>
+
+
+#ifndef HASH_H
+#define HASH_H
+
+
+class hash
+{
+private:
+	static const int  tableSize = 128;
+
+	struct item                 //Word node with its count and a pointer to next
+	{
+		std::string word;
+		int count;
+		item*next;
+		item(){
+			count=0;
+		}
+	};
+
+	item* HashTable[tableSize]; 
+
+public: 
+	hash();
+	
+	int hashFunct(std::string word);
+	void addItem(std::string);
+	void print();
+	
+};
+
+#endif
